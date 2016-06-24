32f33f70fb9053ee2f56a0967a67fa8ec0afd0d8
Kowus <barnabasnomo@gmail.com>
2016-06-24 00:52:27 +0000

    Jukebox Search Algorithm

--- string-theory/string-search.c
+++ string-theory/string-search.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+
+char tracks[][80] = {"I left my heart in Harvard Med School", "Newark, Newark - a wonderful town", "Dancing with a Dork", "From here to maternity", "The girl from Iwo Jima"};
+
+
+
+void find_track(char word[])
+{
+	int i;
+	for(i = 0; i < 5; i++)
+	{
+		if(strstr(tracks[i], word))
+			printf("Track %i: '%s'\n", i, tracks[i]);
+		
+	}
+}
 
 int main()
-{
-	char tracks[][80] = {"I left my heart in Harvard Med School", "Newark, Newark - a wonderful town", "Dancing with a Dork", "From here to maternity", "The girl from Iwo Jima"}
-
-
-
+{	
+	char search_for[80];
+	printf("Search for: ");
+	fgets(search_for, 80, stdin);
+		
+	find_track(search_for);
+	
+	
 	return 0;
 }
+
+
